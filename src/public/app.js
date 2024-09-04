e5f51e017efecffa8627583cec47bd6e5a0b924a
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-09-04 16:50:29 -0400

    UPdated background of draggable box and added assets

--- src/public/app.js
+++ src/public/app.js
@@ -12,94 +12,115 @@ const population = document.getElementById('pop');
 const loadingIndicator = document.getElementById('loading');
 loadingIndicator.style.display = 'none';
 
-paths.forEach(country =>{
-    country.addEventListener("click", function(event){
+function fetchCountryData(name) {
+    // Show loading indicator
+    loadingIndicator.style.display = 'block';
 
-        let name = country.getAttribute('name'); 
+    // Hide the rest of the content
+    document.querySelector('.hidden-content').style.display = 'none';
+
+    countryName.textContent = name;
+    if (name == "United States") {
+        name = "United States of America";
+    }
+
+    // Send the country name to the server
+    fetch('/country', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name })
+    })
+    .then(response => {
+        // Check if the response is ok
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const { countryCapital, date, hours, minutes, weekDay, temp, icon, weatherDesc, pop } = data;
+        if (name == "United States of America") {
+            name = "United States";
+        }
+        countryName.textContent = `${name}, ${countryCapital[0]}`;
+        capitalName.textContent = `${countryCapital[0]}`;
+        capitalTime.textContent = `${getTime(hours, minutes)}`;
+        capitalDate.textContent = `${formatDate(date, weekDay)}`;
+        weather_Desc.textContent = `${weatherDesc}`;
+        weatherIcon.src = `${getIcon(icon)}`;
+        cityTemp.textContent = `${getCelsius(temp)}`;
+        population.textContent = `${pop}`;
+        loadingIndicator.style.display = 'none';
+
+        // Show the rest of the content
+        document.querySelector('.hidden-content').style.display = 'block';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        capitalName.textContent = 'Failed to load capital';
+        capitalTime.textContent = 'Failed to load time';
+    });
+}
+
+paths.forEach(country => {
+    country.addEventListener("click", function(event) {
+        let name = country.getAttribute('name');
         if (name === null || name.trim() === '') {
-            name = country.getAttribute('class');  
+            name = country.getAttribute('class');
         }
-
-        // Show loading indicator
-        loadingIndicator.style.display = 'block';
-
-        // Hide the rest of the content
-        document.querySelector('.hidden-content').style.display = 'none';
-
-        countryName.textContent= name;
-        if(name == "United States"){
-            name = "United States of America";
-        }
-        
-        // Send the country name to the server
-        fetch('/country', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name: name })
-        })
-        .then(response => {
-            // Check if the response is ok
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); 
-        })
-        .then(data => {
-            const {countryCapital, date, hours, minutes, weekDay, temp, icon, weatherDesc, pop} = data;
-            if(name == "United States of America"){
-                name = "United States";
-            }
-            countryName.textContent = `${name}, ${countryCapital[0]}`;
-            capitalName.textContent = `${countryCapital[0]}`;
-            capitalTime.textContent = `${getTime(hours,minutes)}`;
-            capitalDate.textContent = `${formatDate(date, weekDay)}`;
-            weather_Desc.textContent = `${weatherDesc}`;
-            weatherIcon.src = `${getIcon(icon)}`;
-            cityTemp.textContent = `${getCelsius(temp)}°C`;
-            population.textContent = `${pop}`;
-            loadingIndicator.style.display = 'none';
-
-            // Show the rest of the content
-            document.querySelector('.hidden-content').style.display = 'block';
-
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            capitalName.textContent = 'Failed to load capital';
-            capitalTime.textContent = 'Failed to load time';
-        });
+        fetchCountryData(name);
     });
 });
+
+window.onload = function() {
+    fetchCountryData('Canada');
+};
 
 function getIcon(icon){
     switch (icon) {
         case "01d":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/sun.gif')";
             return "images/sun.png";
         case "01n":
+            //fff7fe
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/moon.gif')";
             return "images/moon.png";
         case "02d":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/cloudSun.gif')";
             return "images/cloudSun.png";
         case "02n":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/cloudMoon.gif')";
             return "images/cloudMoon.png";
         case "03d" || "03n":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/cloud.gif')";
             return "images/cloud.png";
         case "04d" || "04n":
+            //#ffe5dc
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/brokenClouds.gif')";
             return "images/brokenClouds.png";
         case "09d" || "09n":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/rain.gif')";
             return "images/rain.png";
         case "10d":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/sunRain.gif')";
             return "images/sunRain.png";
         case "10n":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/moonRain.gif')";
             return "images/moonRain.png";
         case "11d" || "11n":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/storm.gif')";
             return "images/storm.png";
         case "13d" || "13n":
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/snow.gif')";
             return "images/snow.png";
         case "50d" || "50n":
+            //0a7174
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/mist.gif')";
             return "images/mist.png";
         default:
+            document.getElementById('weatherBG').style.backgroundImage = "url('gifs/mist.gif')";
             return "images/mist.png";
     }
 }
@@ -108,10 +129,10 @@ function getCelsius(temp){
     if(temp != undefined){
         console.log(temp);
         const celsius = Number(temp)-273.15;
-        return Math.round(celsius * 10) / 10;
+        return Math.round(celsius * 10) / 10+"°C";
     }
     else{
-        return "?"
+        return "NA"
     }
 }
 
@@ -173,15 +194,15 @@ function onDrag(event) {
     const deltaY = event.clientY - startY;
 
     // Update the transform property to move the element
-    wrapper.style.transform = `translate(${initialTransformX + deltaX}px, ${initialTransformY + deltaY}px)`;
+    wrapper.style.left = `${initialX + deltaX}px`;
+    wrapper.style.top = `${initialY + deltaY}px`;
 }
 
 header.addEventListener("mousedown", (event) => {
-    // Get the current transform values
-    const style = window.getComputedStyle(wrapper);
-    const matrix = new DOMMatrix(style.transform);
-    initialTransformX = matrix.m41 || 0;
-    initialTransformY = matrix.m42 || 0;
+
+    const rect = wrapper.getBoundingClientRect();
+    initialX = rect.left;
+    initialY = rect.top;
 
     startX = event.clientX;
     startY = event.clientY;
