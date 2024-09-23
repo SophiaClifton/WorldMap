86722a5bd7cd40560432f86c46ad34b3c0dd6a88
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-09-22 21:35:40 -0400

    new mysql db needs ssl certification

--- src/server.js
+++ src/server.js
@@ -20,7 +20,10 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        ca: process.env.DB_CA_CERT.replace(/\\n/g, '\n') 
+    }
   });
 connection.connect((err) => {
 if (err) {
