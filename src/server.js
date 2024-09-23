8a0677b3ad80e6b2812e1a1f8f74e482c388080f
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-09-22 21:39:42 -0400

    testing

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     ssl: {
-        ca: process.env.DB_CA_CERT
+        ca: process.env.DB_CA_CERT.replace(/\\n/g, '\n') 
     }
   });
 connection.connect((err) => {
